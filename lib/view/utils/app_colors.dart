2aaea2b39e4138960cedf613a2ac59cdcec7767d
reinanS <reinandesouza01@gmail.com>
2021-11-15 19:13:58 -0300

    feat: add AppColors

---
+++ lib/view/utils/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static final Color white = Color(0xFFFFFFFF);
+  static const Color black = const Color(0xFF00384C);
+  static final Color grey = Color(0xFF626b73);
+  static final Color lightGrey = Color(0xFFf1f3f5);
+  static final Color border = Color(0xFFE1E1E6);
+  static const Color whiteSmoke = const Color(0xFFF5F5F5);
+  static const Color textDark = const Color(0xFF343A40);
+  static const Color gray = const Color(0xFFF1F3F5);
+}
