2e3d7a7fbe367f7d62dba3ad8a3ab381d4d946fc
reinanS <reinandesouza01@gmail.com>
2021-11-15 19:24:49 -0300

    ref: updates main files

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,17 @@
+import 'package:cubos_movies/view/screens/home_screen.dart';
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(AppWidget());
+}
+
+class AppWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "CubosMovies",
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
