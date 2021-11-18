871c02ac2877ac516af793fd6162e7df3ded9994
reinanS <reinandesouza01@gmail.com>
2021-11-18 20:36:07 -0300

    fix: remove ChangeNotifier wrong extend

--- lib/view_model/movie_view_model.dart
+++ lib/view_model/movie_view_model.dart
@@ -4,95 +4,82 @@ import 'package:cubos_movies/model/movie.dart';
 import 'package:cubos_movies/model/repository/movie_repository.dart';
 import 'package:flutter/cupertino.dart';
 
-class MovieViewModel extends ChangeNotifier {
+class MovieViewModel {
   final apiResponse =
       ValueNotifier<ApiResponse>(ApiResponse.initial('Empty data'));
+
+  set response(ApiResponse response) => apiResponse.value = response;
+  ApiResponse get response => apiResponse.value;
 
   // Movie? _movie;
   List<Movie>? _movieList;
   List<Genre>? _genres;
 
-  set response(ApiResponse response) => apiResponse.value = response;
-  ApiResponse get response => apiResponse.value;
-
-  // Movie? get movie => _movie;
-
-  Future<void> init() async {
-    await this.fetchGenresList();
-    // await this.fetchMovieData();
-  }
+  final repository = MovieRepository();
 
   Future<void> fetchMovieData() async {
     response = ApiResponse.loading('Fetching movie data');
-    notifyListeners();
 
     try {
-      _movieList = await MovieRepository().fetchMovieList();
+      _movieList = await repository.fetchMovieList();
       response = ApiResponse.completed(_movieList);
     } catch (e) {
       response = ApiResponse.error(e.toString());
       print(e);
     }
-    notifyListeners();
   }
 
   Future<void> fetchMovieDetailsData(int movieId) async {
     response = ApiResponse.loading('Fetching movie data');
-    notifyListeners();
 
     try {
-      final movie = await MovieRepository().fetchMovieDetails(movieId);
+      final movie = await repository.fetchMovieDetails(movieId);
       response = ApiResponse.completed(movie);
     } catch (e) {
       response = ApiResponse.error(e.toString());
       print(e);
     }
-    notifyListeners();
   }
 
   Future<void> fetchGenresList() async {
     response = ApiResponse.loading('Fetching movie data');
-    notifyListeners();
 
     try {
-      _genres = await MovieRepository().fetchGenreList();
+      _genres = await repository.fetchGenreList();
       response = ApiResponse.completed(_genres);
     } catch (e) {
       response = ApiResponse.error(e.toString());
       print(e);
     }
-    notifyListeners();
   }
 
   Future<void> fetchMoviesByGenre(int genreId) async {
     response = ApiResponse.loading('Fetching movie data');
-    notifyListeners();
 
     try {
-      final movies = await MovieRepository().fetchMoviesByGenre(genreId);
+      final movies = await repository.fetchMoviesByGenre(genreId);
       response = ApiResponse.completed(movies);
     } catch (e) {
       response = ApiResponse.error(e.toString());
       print(e);
     }
-    notifyListeners();
   }
 
   String movieGenderPoster(Movie movie) {
     String posterGenres = '';
 
-    for (int index = 0; index < movie.genreIds.length; index++) {
-      if (index == movie.genreIds.length - 1) {
-        posterGenres += _genres!
-            .firstWhere((genre) => genre.id == movie.genreIds[index])
-            .name;
-      } else {
-        posterGenres += _genres!
-                .firstWhere((genre) => genre.id == movie.genreIds[index])
-                .name +
-            ' - ';
-      }
-    }
+    // for (int index = 0; index < movie.genreIds.length; index++) {
+    //   if (index == movie.genreIds.length - 1) {
+    //     posterGenres += _genres!
+    //         .firstWhere((genre) => genre.id == movie.genreIds[index])
+    //         .name;
+    //   } else {
+    //     posterGenres += _genres!
+    //             .firstWhere((genre) => genre.id == movie.genreIds[index])
+    //             .name +
+    //         ' - ';
+    //   }
+    // }
 
     return posterGenres;
   }
