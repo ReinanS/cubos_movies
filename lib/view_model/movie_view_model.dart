7de28af3ab31190940aae78686aa2347e3778129
reinanS <reinandesouza01@gmail.com>
2021-11-21 10:04:10 -0300

    refactor: MovieViewModel dependency injection

--- lib/view_model/movie_view_model.dart
+++ lib/view_model/movie_view_model.dart
@@ -1,22 +1,22 @@
 import 'package:cubos_movies/model/apis/api_response.dart';
-import 'package:cubos_movies/model/movie.dart';
-import 'package:cubos_movies/model/repository/movie_repository.dart';
 import 'package:flutter/cupertino.dart';
 
 class MovieViewModel {
+  final _repository;
+
+  MovieViewModel(this._repository);
+
   final apiResponse =
       ValueNotifier<ApiResponse>(ApiResponse.initial('Empty data'));
 
   set response(ApiResponse response) => apiResponse.value = response;
   ApiResponse get response => apiResponse.value;
 
-  final repository = MovieRepository();
-
   Future<void> fetchMovieData() async {
     response = ApiResponse.loading('Fetching movie data');
 
     try {
-      final movies = await repository.fetchMovieList();
+      final movies = await _repository.fetchMovieList();
       response = ApiResponse.completed(movies);
     } catch (e) {
       response = ApiResponse.error(e.toString());
@@ -28,7 +28,7 @@ class MovieViewModel {
     response = ApiResponse.loading('Fetching movie data');
 
     try {
-      final movie = await repository.fetchMovieDetails(movieId);
+      final movie = await _repository.fetchMovieDetails(movieId);
       response = ApiResponse.completed(movie);
     } catch (e) {
       response = ApiResponse.error(e.toString());
@@ -40,35 +40,11 @@ class MovieViewModel {
     response = ApiResponse.loading('Fetching movie data');
 
     try {
-      final genres = await repository.fetchGenreList();
+      final genres = await _repository.fetchGenreList();
       response = ApiResponse.completed(genres);
     } catch (e) {
       response = ApiResponse.error(e.toString());
       print(e);
     }
   }
-
-  String movieGenderPoster(Movie movie) {
-    String posterGenres = '';
-
-    // for (int index = 0; index < movie.genreIds.length; index++) {
-    //   if (index == movie.genreIds.length - 1) {
-    //     posterGenres += _genres!
-    //         .firstWhere((genre) => genre.id == movie.genreIds[index])
-    //         .name;
-    //   } else {
-    //     posterGenres += _genres!
-    //             .firstWhere((genre) => genre.id == movie.genreIds[index])
-    //             .name +
-    //         ' - ';
-    //   }
-    // }
-
-    return posterGenres;
-  }
-
-  // void setSelectedMovie(Movie? movie) {
-  //   _movie = movie;
-  //   notifyListeners();
-  // }
 }
