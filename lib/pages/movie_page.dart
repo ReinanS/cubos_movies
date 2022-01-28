0f06cb6c3f3b60e539410ea5658eefd61378406e
reinans <reinandesouza01@gmail.com>
2022-01-28 15:37:11 -0300

    refactor: change movies count in list

--- lib/pages/movie_page.dart
+++ lib/pages/movie_page.dart
@@ -1,4 +1,7 @@
+import 'dart:developer';
+
 import 'package:cubos_movies/core/constant.dart';
+import 'package:cubos_movies/view/widgets/movie_card_widget.dart';
 import 'package:flutter/material.dart';
 import '../widgets/centered_message.dart';
 import '../widgets/centered_progress.dart';
@@ -22,12 +25,22 @@ class _MoviePageState extends State<MoviePage> {
     _initialize();
   }
 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: _buildAppBar(),
+      body: _buildMovieList(),
+    );
+  }
+
   _initScrollListener() {
     _scrollController.addListener(() async {
       if (_scrollController.offset >=
           _scrollController.position.maxScrollExtent) {
         if (_controller.currentPage == lastPage) {
           lastPage++;
+          log(lastPage.toString());
           await _controller.fetchAllMovies(page: lastPage);
           setState(() {});
         }
@@ -35,7 +48,7 @@ class _MoviePageState extends State<MoviePage> {
     });
   }
 
-  _initialize() async {
+  Future<void> _initialize() async {
     setState(() {
       _controller.loading = true;
     });
@@ -47,67 +60,43 @@ class _MoviePageState extends State<MoviePage> {
     });
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: _buildAppBar(),
-      body: _buildMovieGrid(),
-    );
-  }
-
   _buildAppBar() {
     return AppBar(
       backgroundColor: Colors.white,
       elevation: 0,
       title: Text(Constant.kAppName, style: TextStyle(color: Colors.black)),
-      actions: [
-        IconButton(
-          icon: Icon(Icons.refresh),
-          onPressed: _initialize,
-          color: Colors.black,
-        ),
-      ],
     );
   }
 
-  _buildMovieGrid() {
+  Widget _buildMovieList() {
     if (_controller.loading) {
       return CenteredProgress();
     }
 
-    if (_controller.movieError != null) {
+    if (_controller.movieError != null || !_controller.hasMovies) {
       return CenteredMessage(message: _controller.movieError!.message!);
     }
 
-    return GridView.builder(
-      controller: _scrollController,
-      padding: const EdgeInsets.all(2.0),
-      itemCount: _controller.moviesCount,
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        mainAxisSpacing: 2,
-        crossAxisSpacing: 2,
-        childAspectRatio: 0.65,
+    return RefreshIndicator(
+      onRefresh: _initialize,
+      child: GridView.builder(
+        controller: _scrollController,
+        padding: const EdgeInsets.all(2.0),
+        itemCount: _controller.moviesCount,
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 1,
+          mainAxisSpacing: 2,
+          childAspectRatio: 0.65,
+        ),
+        itemBuilder: _buildMovieCard,
       ),
-      itemBuilder: _buildMovieCard,
     );
   }
 
   Widget _buildMovieCard(context, index) {
     final movie = _controller.movies[index];
-    return MovieCard(
-      posterPath: movie.posterPath!,
-      onTap: () => print(movie.title),
+    return MovieCardWidget(
+      movie: movie,
     );
   }
-
-  // _openDetailPage(movieId) {
-  //   Navigator.push(
-  //     context,
-  //     MaterialPageRoute(
-  //       builder: (context) => MovieDetailPage(movieId),
-  //     ),
-  //   );
-  // }
 }
