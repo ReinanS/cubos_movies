4df6c1f55a9f3e1ad959320b3f5d0b6e62a000e8
reinans <reinandesouza01@gmail.com>
2022-02-28 22:50:07 -0300

    feat(inteface): genres repository

--- lib/repositories/genres_repository_imp.dart
+++ lib/repositories/genres_repository_imp.dart
@@ -1,15 +1,20 @@
 import 'package:cubos_movies/core/api.dart';
 import 'package:cubos_movies/errors/movie.error.dart';
 import 'package:cubos_movies/model/movie_genre.dart';
+import 'package:cubos_movies/repositories/genres_repository.dart';
+import 'package:cubos_movies/service/dio_service.dart';
 import 'package:dartz/dartz.dart';
 import 'package:dio/dio.dart';
 
-class GenreRepository {
-  final Dio _dio = Dio(TmdbApi.kDioOptions);
+class GenresRepositoryImp extends GenresRepository {
+  final DioService _dioService;
 
-  Future<Either<MovieError, List<MovieGenre>>> fetchAllGenres() async {
+  GenresRepositoryImp(this._dioService);
+
+  @override
+  Future<Either<MovieError, List<MovieGenre>>> getGenres() async {
     try {
-      final response = await _dio.get('/genre/movie/list');
+      final response = await _dioService.getDio().get('/genre/movie/list');
       final jsonData = response.data['genres'] as List;
 
       List<MovieGenre> genresList =
