d65a119ed8737b6fdd797267768a824a487067a3
reinans <reinandesouza01@gmail.com>
2022-03-05 11:56:24 -0300

    refactor: change files

---
+++ lib/repositories/movies/movies_repository.dart
@@ -0,0 +1,13 @@
+import 'package:cubos_movies/errors/movie.error.dart';
+import 'package:cubos_movies/model/movie_detail_model.dart';
+import 'package:cubos_movies/model/movie_response_model.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class MoviesRepository {
+  Future<Either<MovieError, MovieResponseModel>> getMovies(int page);
+  Future<Either<MovieError, MovieResponseModel>> getMoviesByname(
+      int page, String query);
+  Future<Either<MovieError, MovieResponseModel>> getMovieByGenre(
+      int page, int genre);
+  Future<Either<MovieError, MovieDetailModel>> getMovieById(int id);
+}
--- lib/repositories/movies/movies_repository_imp.dart
+++ lib/repositories/movies/movies_repository_imp.dart
@@ -1,14 +1,17 @@
 import 'dart:developer';
 
 import 'package:cubos_movies/core/api.dart';
+import 'package:cubos_movies/model/movie_detail_model.dart';
 import 'package:cubos_movies/model/movie_response_model.dart';
 import 'package:cubos_movies/errors/movie.error.dart';
-import 'package:cubos_movies/repositories/movies_repository.dart';
+import 'package:cubos_movies/repositories/movies/movies_repository.dart';
 import 'package:cubos_movies/service/dio_service.dart';
 import 'package:dartz/dartz.dart';
 import 'package:dio/dio.dart';
 
 class MoviesRepositoryImp extends MoviesRepository {
+  // SOLID - 5
+  // INVERSÃO DO CONTROLE DE DEPENDENCIA
   final DioService _dioService;
 
   MoviesRepositoryImp(this._dioService);
@@ -16,6 +19,38 @@ class MoviesRepositoryImp extends MoviesRepository {
   @override
   Future<Either<MovieError, MovieResponseModel>> getMovies(int page) async {
     return _getMoviesResponse('/movie/popular?page=$page');
+  }
+
+  @override
+  Future<Either<MovieError, MovieResponseModel>> getMoviesByname(
+      int page, String query) {
+    return _getMoviesResponse('/search/movie?page=$page&query=$query');
+  }
+
+  @override
+  Future<Either<MovieError, MovieResponseModel>> getMovieByGenre(
+      int page, int genre) {
+    return _getMoviesResponse('/movie/popular?page=$page&with_genres=$genre');
+  }
+
+  @override
+  Future<Either<MovieError, MovieDetailModel>> getMovieById(int id) async {
+    try {
+      final response = await _dioService.getDio().get('/movie/$id');
+      final model = MovieDetailModel.fromMap(response.data);
+      log(response.statusCode.toString());
+
+      return Right(model);
+    } on DioError catch (error) {
+      if (error.response == null) {
+        return Left(MovieRepositoryError(TmdbApi.kServerError));
+      } else {
+        return Left(MovieRepositoryError(
+            error.response!.data['status_messsage'].toString()));
+      }
+    } on Exception catch (error) {
+      return Left(MovieRepositoryError(error.toString()));
+    }
   }
 
   Future<Either<MovieError, MovieResponseModel>> _getMoviesResponse(
